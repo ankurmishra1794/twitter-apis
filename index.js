7fdf1a816d768fbee31fa2dd7cbcf0859493dde2
Mishra, Ankur <ankur.e.mishra@accenture.com>
2024-09-27 11:33:37 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+import dotenv from 'dotenv';
+import express from 'express';
+import { DBCon } from './config/db.js';
+import userRoute from './routes/userRoute.js'
+import tweetRoute from './routes/tweetRoute.js'
+import cookieParser from 'cookie-parser';
+import cors from 'cors'
+
+dotenv.config({
+    path: '.env'
+});
+
+const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.use(cors({
+    origin: "http://localhost:3000",
+    credentials: true
+}));
+
+app.use("/api/v1/user", userRoute)
+app.use("/api/v1/tweet", tweetRoute)
+
+app.listen(process.env.PORT, () => {
+    DBCon();
+    console.log(`Server listening at port ${process.env.PORT}`);
+});
