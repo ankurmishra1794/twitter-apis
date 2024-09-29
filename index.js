5be8cad8f67b3f70f4ba00dd0b8ac0d364a25a63
Mishra, Ankur <ankur.e.mishra@accenture.com>
2024-09-30 01:00:55 +0530

    origin url changed

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json());
 app.use(cookieParser());
 
 app.use(cors({
-    origin: "http://localhost:3000",
+    origin: "https://twitter-apis-4bhr.onrender.com",
     credentials: true
 }));
 
